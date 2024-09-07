d7599920ffd9c36f61e3f81b682ea3ec17cb396b
keithfykai <68919845+keithfykai@users.noreply.github.com>
2024-09-08 00:41:58 +0800

    test

---
+++ keith_lim_portfolio/src/App.tsx
@@ -0,0 +1,17 @@
+import NavBar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+
+      <Hero></Hero>
+
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
