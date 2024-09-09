ee30ccf9ebd32ea9f9801eda3346c5602cc91f10
keithfykai <68919845+keithfykai@users.noreply.github.com>
2024-09-10 01:09:03 +0800

    progress

--- keith_lim_portfolio/src/App.tsx
+++ keith_lim_portfolio/src/App.tsx
@@ -1,17 +1,34 @@
-import NavBar from "./components/Navbar"
-import Footer from "./components/Footer"
-import Hero from "./components/Hero"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import AboutMe from "./components/pages/AboutMe"; 
+import Projects from './components/pages/Projects';
+import Travels from './components/pages/Travels';
+import Blogs from './components/pages/Blogs';
 
 function App() {
   return (
-    <>
-      <NavBar></NavBar>
+    <Router>
+      <div>
+        <NavBar />
+        {/* Wrap all your routes in the Routes component */}
+        <Routes>
+          {/* Define the default route for the home page */}
+          <Route path="/" element={<Hero />} />
 
-      <Hero></Hero>
+          {/* Define additional routes for other pages */}
+          <Route path="/aboutme" element={<AboutMe />} />
+          <Route path="/travels" element={<Travels />} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/blogs" element={<Blogs />} />
 
-      <Footer></Footer>
-    </>
-  )
+          {/* Add more routes as necessary */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
