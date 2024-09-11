57bd9aae50d96efae327fcecdcb4bfc1c420a159
keithfykai <68919845+keithfykai@users.noreply.github.com>
2024-09-11 12:05:31 +0800

    test commit pages

---
+++ src/components/pages/Projects.tsx
@@ -0,0 +1,20 @@
+import projectCard from "../projects/projectCard"
+
+export default function Projects(){
+    return (
+        <>
+            <div>
+                <div className="bg-white py-20">
+                    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl py-2">
+                            Projects
+                        </h1>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
