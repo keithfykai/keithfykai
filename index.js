bf663429d4998307964a673ecdff1bfb927b0e43
keithfykai <keithfy123@gmail.com>
2025-05-09 20:25:48 +0200

    helo

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from 'inquirer';
+import { image, imageSync } from 'qr-image'
+ 
+
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    "hello?"
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
